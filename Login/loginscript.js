d90e3ce36c0ed9760c6284cb184be7b2ea07ec02
SupremeWarMan <146172547+SupremeWarMan@users.noreply.github.com>
2023-10-14 13:26:06 +0000

    V23

---
+++ Login/loginscript.js
@@ -0,0 +1,11 @@
+type="text/javascript">
+function checkPswd() {
+    var confirmPassword = "admin";
+    var password = document.getElementById("pswd").value;
+    if (password == confirmPassword) {
+         window.location="protected.html";
+    }
+    else{
+        alert("Passwords do not match.");
+    }
+}
